7fd9db7e5a4d316b296ffe83f00f5c00ad45e000
NicolyBaesso <nicolybaesso18@gmail.com>
2025-04-10 09:50:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+//JS - JavaScript
+
+var pais = ["Rússia","Canadá", "China", "Estados Unidos","Austrália", "Brasil", "Índia", "Argentina", "Cazaquistão", "Argélia"]
+var paisEscolhido = parseInt(prompt("Digite o número 0 a 9"))
+document.write("País Escolhido:" + pais[paisEscolhido])
